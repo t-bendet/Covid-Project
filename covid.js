50b258a7eeb665cfe51bf6dfbe5fce21427ef39d
t-bendet <talbendet21@gmail.com>
2021-03-05 21:09:24 +0200

    data fetching and filter done

--- covid.js
+++ covid.js
@@ -1,21 +1,69 @@
-var ctx = document.getElementById("myChart").getContext("2d");
-var chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: "line",
+async function getInitialData() {
+  const covidPromise = await fetch("https://corona-api.com/countries");
+  const covidData = await covidPromise.json(); //array of objects covidData.data[0]
+  const countriesPromise = await fetch(
+    "https://api.codetabs.com/v1/proxy?quest=https://restcountries.herokuapp.com/api/v1"
+  );
 
-  // The data for our dataset
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 20, 5, 2, 20, 30, 50],
-      },
-    ],
-  },
+  const countriesData = await countriesPromise.json(); //array of objects countriesData[x]
+  return [covidData.data, countriesData];
+}
+//
+async function CreateDataObject() {
+  // creating world
+  const world = new Map([
+    ["Asia", new Map([])],
+    ["Europe", new Map([])],
+    ["Africa", new Map([])],
+    ["Americas", new Map([])],
+    ["Oceania", new Map([])],
+    ["Who Lives Here?", new Map([])],
+  ]);
+  const [covidArray, countriesArray] = await getInitialData();
+  // if there is a name for the region place in proper region,if not put in "Who Lives Here?"
+  for (const countrie of countriesArray) {
+    let trimName = countrie.name.common.trim();
+    if (countrie.region) {
+      world.get(countrie.region).set(trimName, new Map([]));
+      world.get(countrie.region).get(trimName).set("capitel", countrie.capital);
+      world.get(countrie.region).get(trimName).set("region", countrie.region);
+      world.get(countrie.region).get(trimName).set("borders", countrie.borders);
+      world.get(countrie.region).get(trimName).set("TLD", countrie.tld[0]);
+    } else {
+      world.get("Who Lives Here?").set(trimName, new Map([]));
+      world
+        .get("Who Lives Here?")
+        .get(trimName)
+        .set("capitel", countrie.capital);
+      world.get("Who Lives Here?").get(trimName).set("region", countrie.region);
+      world
+        .get("Who Lives Here?")
+        .get(trimName)
+        .set("borders", countrie.borders);
+      world.get("Who Lives Here?").get(trimName).set("TLD", countrie.tld[0]);
+    }
+  }
+  // looping over world regions and asaiging covid info to each country
+  for (const info of covidArray) {
+    for (const [region, countries] of world) {
+      for (const [cont, val] of countries) {
+        let conTLD = val.get("TLD");
+        if (conTLD) {
+          let parm = conTLD.slice(1).toUpperCase();
+          if (info.code == parm) {
+            val.set("info", info);
+            // console.log(val)
+          }
+        }
+      }
+    }
+  }
+  // TODO  "Europe -Kosovo, United Kingdom","Americas -Bonaire"add contries
+  return world;
+}
 
-  // Configuration options go here
-  options: {},
-});
+async function mainData() {
+  const worldMap = await CreateDataObject();
+  console.log(worldMap.get("Asia"));
+}
+mainData();
--- project guidelines/pseudo code.md
+++ project guidelines/pseudo code.md
@@ -1,28 +1,30 @@
 ## fetching data
 
-- [ ] postmen :figure out how the data is structured
-- [ ] async functions
-- [ ] fetch countries
-- [ ] fetch covid
-- [ ] Q:CORS problems?
+- [x] postmen :figure out how the data is structured
+- [x] fetch countries
+- [x] fetch covid
+- [x] Q:CORS problems?
 
 ## filtering data
 
-- [ ] Q: what is the data i need?
-- [ ] Q: what is the most dynemic way to store and access it?
-- [ ] Q: how chart j.s should gets the info?
-- [ ] divide to contents total cases,new cases,total deaths,new deaths,total recovered,in critical condition,
-- [ ] conteries :,Confirmed Cases,Number of Deaths,Number of recovered,Number of critical condition
+- [x] Q: what is the data i need?
+- [x]A: data obj by world : {region(asia):{conterie(israel):{...relevent data}}}
+- [x] Q: what is the most dynemic way to store and access it?
+- [x] A:map,slower storing time,faster access , maintain entry order insertion.
+- [x] Q: how chart j.s should gets the info?
+- [x] A: arrays
 
 ## formating data for libaries
 
 - [ ] functions for getting data (africa,confirmed cases) (isreal,all parms.....)
+- [ ] divide to Regions total cases,new cases,total deaths,new deaths,total recovered,in critical condition,
+- [ ] conteries :Confirmed Cases,Number of Deaths,Number of recovered,Number of critical condition
 - [ ] injecting data into chart js
 
 ## create elements and display data
 
 - [ ] hard code html general structure
-- [ ] Q:what is created on the fky with j.s?
+- [ ] Q:what is created on the fly with j.s?
 - [ ] Q:true/false for contennint btn?
 - [ ] manipulating data into the dom with pre-build grid/flex or any other method
 
@@ -35,5 +37,8 @@
 1. Add more features. You can do much more with this API
 2. Make it look pretty
 3. Tweak the performance. Maybe local storage can help us here
-4. Create a spinner when the data is loading.
+4. Create a spinner when the data is loading. or ither cool animation
 5. Disable the buttons when you send an api request. Now what happens is you can click numerous of times on a button and it will send numerous times an api request. But we really only need one.
+6. devision by sub regions
+7. more forms of chrts
+8. adiitional data on contry :contry api (borders,capital),covidapi(updated_at)
